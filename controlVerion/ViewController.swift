d23d4bf57aed4ac85335899bac3117a64e8bb508
raju <araju2014to2018@gmail.com>
2020-01-10 15:52:54 +0530

    hi shanth add

--- controlVerion/ViewController.swift
+++ controlVerion/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         print("hi raju")
+        print("hi mr. shanthkumar")
         // Do any additional setup after loading the view.
     }
 
